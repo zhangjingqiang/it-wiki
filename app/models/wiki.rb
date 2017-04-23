72317b3f9d66023b222c543fd9c70ec32ec9c696
zhangjingqiang <zhangjingqiang@gmail.com>
2017-04-23 21:16:48 +0900

    Add uniqueness validation to link

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,6 +2,6 @@ class Wiki < ApplicationRecord
   acts_as_ordered_taggable
 
   validates :title, presence: true, length: { maximum: 255 }
-  validates :link, presence: true, url: true
+  validates :link, presence: true, uniqueness: true, url: true
   validates :tag_list, presence: true
 end
