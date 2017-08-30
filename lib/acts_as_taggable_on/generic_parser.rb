6df534f76a89e22d547ecbaf3dc1121217bde6a7
zhangjingqiang <zhangjingqiang@gmail.com>
2017-08-30 13:06:08 +0900

    Remove unused class and method

--- lib/acts_as_taggable_on/generic_parser.rb
+++
@@ -1,7 +0,0 @@
-class MyParser < ActsAsTaggableOn::GenericParser
-  def parse
-    ActsAsTaggableOn::TagList.new.tap do |tag_list|
-      tag_list.add @tag_list.split(',').map(&:strip).reject(&:empty?)
-    end
-  end
-end
