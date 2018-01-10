1e78b5cccd036dfefb69153f4fc160b4cc1b02b7
zhangjingqiang <zhangjingqiang@gmail.com>
2018-01-10 13:55:22 +0000

    Add uniq check for url

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,7 +1,13 @@
 class Wiki < ApplicationRecord
   acts_as_ordered_taggable
 
+  before_validation :process_url
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :link, presence: true, uniqueness: true, url: true
   validates :tag_list, presence: true
+
+  def process_url
+    self.link = self.link.slice(0, self.link.length - 1) if self.link.present? && self.link.ends_with?("/")
+  end
 end
